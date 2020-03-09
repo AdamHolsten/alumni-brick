0a3fbc382264ba882b30e147d9afe5ab755538e6
Dancingrobots <maxpower713@gmail.com>
2020-03-09 14:16:51 -0400

    Working Copy

--- final-draft/search-init.js
+++ final-draft/search-init.js
@@ -1,35 +1,42 @@
-
-var options = {
-  valueNames: [ 'project-name', 'project-title', 'project-label' ],
-  page: 2,
-  pagination: {
-    innerWindow: 1,
-    left: 0,
-    right: 0,
-    paginationClass: "pagination",
-    },
-};
-
-var userList = new List('sandbox', options);
-
-
-$('.jPaginateNext').on('click', function(){
-  var list = $('.pagination').find('li');
-  $.each(list, function(position, element){
-      if($(element).is('.active')){
-          $(list[position+1]).trigger('click');
-      }
-  })
+var monkeyList = new List('sandbox', {
+  valueNames: ['project-name', 'project-title', 'project-label'],
+  page: 4,
+  pagination: true
 });
 
 
-$('.jPaginateBack').on('click', function(){
-  var list = $('.pagination').find('li');
-  $.each(list, function(position, element){
-      if($(element).is('.active')){
-          $(list[position-1]).trigger('click');
-      }
-  })
-});
+
+// var options = {
+//   valueNames: [ 'project-name', 'project-title', 'project-label' ],
+//   page: 2,
+//   pagination: {
+//     innerWindow: 1,
+//     left: 0,
+//     right: 0,
+//     paginationClass: "pagination",
+//     },
+// };
+
+// var userList = new List('sandbox', options);
+
+
+// $('.jPaginateNext').on('click', function(){
+//   var list = $('.pagination').find('li');
+//   $.each(list, function(position, element){
+//       if($(element).is('.active')){
+//           $(list[position+1]).trigger('click');
+//       }
+//   })
+// });
+
+
+// $('.jPaginateBack').on('click', function(){
+//   var list = $('.pagination').find('li');
+//   $.each(list, function(position, element){
+//       if($(element).is('.active')){
+//           $(list[position-1]).trigger('click');
+//       }
+//   })
+// });
 
 
