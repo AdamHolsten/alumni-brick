80b19c334d2809e02ab71f00fd04acb19f8c593e
lvl12sealclubber <maxpower713@gmail.com>
2020-03-24 13:38:41 -0400

    Final Working

---
+++ live-verion-testing/js/search-init-final3.js
@@ -0,0 +1,28 @@
+var monkeyList = new List('sandbox', {
+  valueNames: ['project-name', 'project-title', 'project-label'],
+  page: 4,
+  pagination: true
+});
+
+
+var userList = new List('users', monkeyList);
+userList.on('updated', function(list) {
+  if (list.matchingItems.length > 0) {
+    document.getElementsByClassName('no-result').hide()
+  } else {
+    document.getElementsByClassName('no-result').show()
+  }
+})
+
+$('#sort').change(function () {
+  var selection = this.value;
+  if (selection) {
+      featureList.filter(function (item) {
+          return (item.values().material == selection);
+      });
+  } else {
+      featureList.filter();
+  }
+});
+
+
